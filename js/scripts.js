d5e1e08002937f6b404ea16573495a6835bca641
Colton Lacey and dominic montelongo <colton@>
2019-04-04 16:34:38 -0700

    fix function to convert numbers to roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,67 @@
-function toRomanSingle(num){
-    if (num === 1){
-    num = 'I'
-  } else if (num === 2 ){
-    num = 'II'
-  } else if (num === 3){
-    num = 'III'
-  }  else if (num === 4) {
-    num = 'IV'
-  } else if (num === 5) {
-    num = 'V'
-  } else if (num === 6) {
-    num = 'VI'
-  } else if (num === 7){
-    num = 'VII'
-  } else if (num === 8) {
-    num = 'VIII'
-  } else if (num === 9) {
-    num = 'IX'
-  } else if (num === 10){
-    num = 'X'
-  } else {
-    num = ''
-  }
-  return num;
-}
 
-var romanNumeralArray = []
+var romanNumeralOutput = [];
 
 function toRomanNumeral(num){
-  for (var i = num; i !===0; i += 0) {
-  if (num > 4000) {
-  return "Enter amount smaller than 4000";
-  } else if (num === 1,000){
+  for (var i = num; i != 0; i += 0) {
+  // if (i > 4000) {
+  // return "Enter amount smaller than 4000";
+  if (i >= 1000){
     romanNumeralOutput.push("M");
-
-  } else if (num === 900) {
+    var i = (i - 1000);
+      var num = i;
+  } else if (i >= 900) {
     romanNumeralOutput.push("CM");
-  } else if (num === 500) {
+    var i = (i - 900);
+    var num = i;
+  } else if (i >= 500) {
     romanNumeralOutput.push("D");
-  } else if  (num === 400){
+    var i = (i - 500);
+      var num = i;
+  } else if  (i >= 400){
     romanNumeralOutput.push("CD");
-  } else if (num === 100) {
+    var i = (i - 400);
+      var num = i;
+  } else if (i >= 100) {
     romanNumeralOutput.push("C");
-  } else if (num === 90) {
+    var i = (i - 100);
+      var num = i;
+  } else if (i >= 90) {
     romanNumeralOutput.push("XC");
-  } else if (num === 50) {
+    var i = (i - 90);
+      var num = i;
+  } else if (i >= 50) {
     romanNumeralOutput.push("L");
-  }  else if (num === 40) {
+    var i = (i - 50);
+      var num = i;
+  }  else if (i >= 40) {
     romanNumeralOutput.push("XL");
-  } else if (num === 10) {
+    var i = (i - 40);
+      var num = i;
+  } else if (i >= 10) {
     romanNumeralOutput.push("X");
-  } else if (num === 9) {
+    var i = (i - 10);
+      var num = i;
+  } else if (i >= 9) {
     romanNumeralOutput.push("IX");
-  } else if (num === 5) {
+    var i = (i - 9);
+      var num = i;
+  } else if (i >= 5) {
     romanNumeralOutput.push("V");
-  } else if (num === 4){
-    romanNumeralOutput.push("IV")
-  } else if (num === 1) {
-    romanNumeralOutput.push("I")
-  } else {
-    return
+    var i = (i - 5);
+      var num = i;
+  } else if (i >= 4){
+    romanNumeralOutput.push("IV");
+    var i = (i - 4);
+      var num = i;
+  } else if (i >= 1) {
+    romanNumeralOutput.push("I");
+    var i = (i - 1);
+      var num = i;
   }
+
 }
-
-
+}
+var result = romanNumeralOutput.join('');
 
 
 
@@ -72,16 +70,35 @@ $(document).ready(function() {
   $("form#roman-numeral").submit(function(event) {
     event.preventDefault();
     var num = parseInt($("input#number").val());
-    var result = toRomanSingle(num);
-    $("#result").show(result);
-    $(".conversion").text(result);
-
-    console.log(result);
+  toRomanNumeral(num);
+  $(".conversion").text(result);
 
   });
 });
-// var romanNumeralOutput
-// }
-// }
-// //
+
+// function toRomanSingle(num){
+//     if (num === 1){
+//     num = 'I'
+//   } else if (num === 2 ){
+//     num = 'II'
+//   } else if (num === 3){
+//     num = 'III'
+//   }  else if (num === 4) {
+//     num = 'IV'
+//   } else if (num === 5) {
+//     num = 'V'
+//   } else if (num === 6) {
+//     num = 'VI'
+//   } else if (num === 7){
+//     num = 'VII'
+//   } else if (num === 8) {
+//     num = 'VIII'
+//   } else if (num === 9) {
+//     num = 'IX'
+//   } else if (num === 10){
+//     num = 'X'
+//   } else {
+//     num = ''
+//   }
+//   return num;
 // }
